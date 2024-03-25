0b1733a452b2804d532892d6906c3a29bd23d342
ArmandoGonzalez <ajgl0001@ce.pucmm.edu.do>
2024-03-25 19:56:57 -0400

    Volver a version anterior de geoloc

--- src/main/resources/public/js/geolocalizacion.js
+++ src/main/resources/public/js/geolocalizacion.js
@@ -1,18 +1,22 @@
 let lat, lon;
 
 window.onload = function() {
-    // Disable the submit button
-    document.querySelector('button[type="submit"]').disabled = true;
+    console.log("geolocalizacion.js loaded"); // This will log when the script is loaded
 
     if (navigator.geolocation) {
+        console.log("Geolocation API is supported"); // This will log if the Geolocation API is supported
+
         navigator.geolocation.getCurrentPosition(function(position) {
+            document.getElementById('latitud').value = position.coords.latitude;
+            document.getElementById('longitud').value = position.coords.longitude;
+            // Success callback
             lat = position.coords.latitude;
             lon = position.coords.longitude;
 
-            // Enable the submit button
-            document.querySelector('button[type="submit"]').disabled = false;
+            console.log("Location retrieved: ", lat, lon); // This will log the retrieved location
         }, function(error) {
-            console.log("Error retrieving location: ", error);
+            // Error callback
+            console.log("Error retrieving location: ", error); // This will log any errors
         });
     } else {
         console.log("Geolocation is not supported by this browser.");
@@ -20,6 +24,6 @@ window.onload = function() {
 }
 
 function setCoordinates() {
+    // Store the coordinates in the form's hidden input fields
     document.getElementById('latitud').value = lat;
     document.getElementById('longitud').value = lon;
-}
